8a5d1fd7a860d422b99208c84e3c03328cb780d7
tathagatpal <55681355+tathagatpal@users.noreply.github.com>
2021-06-09 00:40:22 +0530

    Add files via upload

---
+++ Dataset/IID_Implementation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  9 00:16:03 2021
+
+@author: tathagat
+"""
+
+from IID_Data import load_dataset, getActualImages
+
+train_dataset, test_dataset, train_group, test_group = load_dataset(10) #Users = 10
+
+first_client_batches = getActualImages(train_dataset, train_group[0], 64)
+
+for images, labels in first_client_batches:
+    print(len(images))
+    break
