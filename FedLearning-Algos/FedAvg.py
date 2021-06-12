46f75f8e18c64cc9504e94c02ed9b19d0dcb6d9b
tathagatpal <55681355+tathagatpal@users.noreply.github.com>
2021-06-13 01:04:57 +0530

    Update FedAvg.py

--- FedLearning-Algos/FedAvg.py
+++ FedLearning-Algos/FedAvg.py
@@ -22,7 +22,7 @@ from NonIID import load_dataset, getActualImages
 from utils import averageModels, averageGradients
 
 '''
-Create a list of clients, where each client will have a dictionary. The dictionaries will have DataLoader, Model etc.
+Create a list of clients, where each client will be a dictionary. The dictionaries will have DataLoader, Model etc.
 '''
 
 class Arguments():
